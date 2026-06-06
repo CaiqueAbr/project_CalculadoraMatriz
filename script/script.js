2deee78be7315b6e4f49c5c78ae5ad17ade433e8
CaiqueAbr <caiqueabranches2009@gmail.com>
2026-06-06 16:07:10 -0300

    Criação das funções que calculam as operações

--- script/script.js
+++ script/script.js
@@ -43,15 +43,29 @@ btnMatrizB.addEventListener("click", toggleMatrizB);
 
 // Verifica se a matrizB já existe ou não quando clica no botão, se existir = apaga, se não, envia para a função de gerar matriz.
 function toggleMatrizB() {
+    const btnAdicao = document.getElementById('btnAdicao');
+    const btnSubtracao = document.getElementById('btnSubtracao');
+    const btnMultAB = document.getElementById('btnMultiplicacaoAB');
+    const btnMultBA = document.getElementById('btnMultiplicacaoBA');
+        
     if (matrizBAtiva === false){
         matrizBAtiva = true;
         btnMatrizB.textContent = '- REMOVER MATRIZ B';
         gerarMatrizB();
+        
+        btnAdicao.style.display = 'block';
+        btnSubtracao.style.display = 'block';
+        btnMultAB.style.display = 'block';
+        btnMultBA.style.display = 'block';
     }
     else {
         matrizBAtiva = false;
         btnMatrizB.textContent = '+ ADICIONAR MATRIZ B';
         sectionEscolhaMatrizB.innerHTML = '';
+        btnAdicao.style.display = 'none';
+        btnSubtracao.style.display = 'none';
+        btnMultAB.style.display = 'none';
+        btnMultBA.style.display = 'none';
     }
 
 }
@@ -88,6 +102,7 @@ function gerarMatrizB() {
 
     const selLinhaMatrizB = document.getElementById('selLinhaMatrizB');
     const selColunaMatrizB = document.getElementById('selColunaMatrizB');
+
     
     selLinhaMatrizB.addEventListener('change', gerarGridMatrizB);
     selColunaMatrizB.addEventListener('change', gerarGridMatrizB);
@@ -119,6 +134,7 @@ function gerarGridMatrizB() {
 
         divMatrizB.appendChild(divLinha);
     }
+
 
 }
 
@@ -153,20 +169,24 @@ btnSelctOperacao.forEach(function(btn) {
         btnSelctOperacao.forEach(function(b) {
             b.classList.remove('btnOperacaoAtivo');
         })
+
         btn.classList.add('btnOperacaoAtivo');
+
+        if (btn.id === 'btnMultEscalar') {
+            document.getElementById('divInputEscalar').style.display = 'block'
+        } else {
+            document.getElementById('divInputEscalar').style.display = 'none'
+        }
     })
 })
 
 //Verifica operação escolhida e envia para a função que calcula tal operaçao.
 btnCalcularOperacao.addEventListener('click', verificarOperacao);
 function verificarOperacao() {
-
-    console.log('A', selLinhaMatrizA.value, selColunaMatrizA.value)
-
     if (operacaoSelecionada === 'btnTransposta'){
         calcularTransposta();
     }
-    else if (operacaoSelecionada === 'btnProdutoAAT') {
+    else if (operacaoSelecionada === 'btnProdutoAAt') {
         calcularProdutoAAT();
     }
     else if (operacaoSelecionada === 'btnProdutoAtA') {
@@ -181,8 +201,40 @@ function verificarOperacao() {
     else if (operacaoSelecionada === 'btnInversa'){
         calcularInversa();
     }
+    else if (operacaoSelecionada === 'btnAdicao'){
+        if (matrizBAtiva === true){
+           calcularAdicao(); 
+        }
+        else{
+            alert('Selecione uma operação');
+        }
+    }
+    else if (operacaoSelecionada === 'btnSubtracao'){
+        if (matrizBAtiva === true){
+            calcularSubtracao();
+        }
+        else{
+            alert('Selecione uma operação');
+        }
+    }
+    else if (operacaoSelecionada === 'btnMultiplicacaoAB'){
+        if (matrizBAtiva){
+            calcularMultiplicacaoAB();
+        }
+        else{
+            alert('Selecione uma operação');
+        }
+        
+    }
+    else if (operacaoSelecionada === 'btnMultiplicacaoBA'){
+        if (matrizBAtiva === true){
+            calcularMultiplicacaoBA();
+        }
+        else{
+            alert("Selecione uma operação")
+        }
+    }
 }
-
 function calcularTransposta() {
     const linhas = parseInt(selLinhaMatrizA.value);
     const colunas = parseInt(selColunaMatrizA.value);
@@ -199,5 +251,219 @@ function calcularTransposta() {
 
         transposta.push(linha)
     }
+
     console.log(transposta);
+    return transposta;
+}
+function calcularProdutoAAT() {
+    console.log('entrou na função!') // adiciona essa linha
+    const linhas = parseInt(selLinhaMatrizA.value);
+    const colunas = parseInt(selColunaMatrizA.value);
+    const matrizA = lerMatriz('A', linhas, colunas);
+    const transposta = calcularTransposta();
+    const matrizAAT = [];
+    
+    for (let i = 0; i < linhas; i++){
+        const linha = [];
+        for (let j = 0; j < linhas; j++){
+            let soma = 0;
+
+            for (let k = 0; k < colunas; k++){
+                soma += matrizA[i][k] * transposta[k][j]
+            }
+            linha.push(soma);
+        }
+        matrizAAT.push(linha)
+    }
+
+    console.log(matrizAAT);
+    return matrizAAT;
+}
+function calcularProdutoAtA() {
+
+    console.log('produto ata')
+    const linhas = parseInt(selLinhaMatrizA.value);
+    const colunas = parseInt(selColunaMatrizA.value);
+    const matrizA = lerMatriz('A', linhas, colunas);
+    const transposta = calcularTransposta();
+    const matrizAtA = [];
+    
+    for (let i = 0; i < colunas; i++){
+        const linha = [];
+        for (let j = 0; j < colunas; j++){
+            let soma = 0;
+
+            for (let k = 0; k < linhas; k++){
+                soma += transposta[i][k] * matrizA[k][j]
+            }
+            linha.push(soma);
+        }
+        matrizAtA.push(linha)
+    }
+
+    console.log(matrizAtA);
+    return matrizAtA;
+}
+function calcularMultEscalar() {
+    console.log("multescalar");
+    const linhas = parseInt(selLinhaMatrizA.value);
+    const colunas = parseInt(selColunaMatrizA.value);
+    const matrizA = lerMatriz('A', linhas, colunas);
+    const valorK = document.getElementById('inputEscalar').value;
+    const matrizMultEscalar = [];
+
+    for (let i = 0; i < linhas; i++){
+        const linha = [];
+        let mult = 0;
+        for (let j = 0; j < colunas; j++){
+            mult = matrizA[i][j] * valorK;
+            linha.push(mult);
+        } 
+        matrizMultEscalar.push(linha)
+    } 
+
+    console.log(matrizMultEscalar);
+    return matrizMultEscalar;
+}
+function calcularAdicao() {
+    const linhasA = parseInt(selLinhaMatrizA.value);
+    const colunasA = parseInt(selColunaMatrizA.value);
+    
+    const selLinhaMatrizB = document.getElementById('selLinhaMatrizB');
+    const selColunaMatrizB = document.getElementById('selColunaMatrizB');
+    const linhasB = parseInt(selLinhaMatrizB.value);
+    const colunasB = parseInt(selColunaMatrizB.value);
+
+    const MatrizA = lerMatriz('A', linhasA, colunasA);
+    const MatrizB = lerMatriz('B', linhasB, colunasB);
+    const matrizSoma = []
+
+    console.log(linhasA, linhasB, colunasA, colunasB)
+    if(linhasA === linhasB && colunasA === colunasB){
+        for (let i = 0; i < linhasA; i++)
+        {
+            let linha = [];
+            for (let j = 0; j < colunasA; j++)
+            {
+                linha.push(MatrizA[i][j] + MatrizB[i][j]);
+            }
+            matrizSoma.push(linha);
+        }
+        console.log(matrizSoma);
+    }
+    else {
+        alert('As matrizes devem ser da mesma dimensão');
+    }
+}
+function calcularSubtracao() {
+    const linhasA = parseInt(selLinhaMatrizA.value);
+    const colunasA = parseInt(selColunaMatrizA.value);
+    
+    const selLinhaMatrizB = document.getElementById('selLinhaMatrizB');
+    const selColunaMatrizB = document.getElementById('selColunaMatrizB');
+    const linhasB = parseInt(selLinhaMatrizB.value);
+    const colunasB = parseInt(selColunaMatrizB.value);
+
+    const MatrizA = lerMatriz('A', linhasA, colunasA);
+    const MatrizB = lerMatriz('B', linhasB, colunasB);
+    const matrizSubtracao = []
+
+    console.log(linhasA, linhasB, colunasA, colunasB)
+    if(linhasA === linhasB && colunasA === colunasB){
+        for (let i = 0; i < linhasA; i++)
+        {
+            let linha = [];
+            for (let j = 0; j < colunasA; j++)
+            {
+                linha.push(MatrizA[i][j] - MatrizB[i][j]);
+            }
+            matrizSubtracao.push(linha);
+        }
+        console.log(matrizSubtracao);
+    }
+    else {
+        alert('As matrizes devem ser da mesma dimensão');
+    }
+}
+function calcularMultiplicacaoAB() {
+    const linhasA = parseInt(selLinhaMatrizA.value);
+    const colunasA = parseInt(selColunaMatrizA.value);
+    
+    const selLinhaMatrizB = document.getElementById('selLinhaMatrizB');
+    const selColunaMatrizB = document.getElementById('selColunaMatrizB');
+    const linhasB = parseInt(selLinhaMatrizB.value);
+    const colunasB = parseInt(selColunaMatrizB.value);
+
+    const MatrizA = lerMatriz('A', linhasA, colunasA);
+    const MatrizB = lerMatriz('B', linhasB, colunasB);
+    const matrizMultAB = []
+
+    console.log(linhasA, linhasB, colunasA, colunasB)
+    if(colunasA === linhasB){
+        for (let i = 0; i < linhasA; i++){
+        const linha = [];
+        for (let j = 0; j < colunasB; j++){
+            let soma = 0;
+
+            for (let k = 0; k < colunasA; k++){
+                soma += MatrizA[i][k] * MatrizB[k][j]
+            }
+            linha.push(soma);
+        }
+        matrizMultAB.push(linha)
+    }
+        console.log(matrizMultAB);
+    }
+    else {
+        alert('As colunas da MatrizA devem ter dimensão igual as dimensões da linhas da MatrizB');
+    }
+}
+function calcularMultiplicacaoBA() {
+    const linhasA = parseInt(selLinhaMatrizA.value);
+    const colunasA = parseInt(selColunaMatrizA.value);
+    
+    const selLinhaMatrizB = document.getElementById('selLinhaMatrizB');
+    const selColunaMatrizB = document.getElementById('selColunaMatrizB');
+    const linhasB = parseInt(selLinhaMatrizB.value);
+    const colunasB = parseInt(selColunaMatrizB.value);
+
+    const MatrizA = lerMatriz('A', linhasA, colunasA);
+    const MatrizB = lerMatriz('B', linhasB, colunasB);
+    const matrizMultBA = []
+
+    console.log(linhasA, linhasB, colunasA, colunasB)
+    if(colunasB === linhasA){
+        for (let i = 0; i < linhasB; i++){
+        const linha = [];
+        for (let j = 0; j < colunasA; j++){
+            let soma = 0;
+
+            for (let k = 0; k < colunasB; k++){
+                soma +=  MatrizB[i][k] * MatrizA[k][j]
+            }
+            linha.push(soma);
+        }
+        matrizMultBA.push(linha)
+    }
+        console.log(matrizMultBA);
+    }
+    else {
+        alert('As colunas da MatrizB devem ter dimensão igual as dimensões da linhas da MatrizA');
+    }
+}
+function calcularDeterminante() {
+    const linhas = parseInt(selLinhaMatrizA.value);
+    const colunas = parseInt(selColunaMatrizA.value);
+    const matriz = lerMatriz('A', linhas, colunas)
+    let det = 0;
+
+    if (linhas === colunas){
+        if (linhas === 2 && colunas === 2){
+            det = matriz[0][0] * matriz[1][1] - matriz[0][1] * matriz[1][0];
+            console.log(det);
+        }
+    }
+    else {
+        alert('O número de colunas e de linhas da Matriz devem ser iguais')
+    }
 }
